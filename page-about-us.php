5443566862692dc820f573964d41dfb072556e91
kejiri <180035898+kejiri@users.noreply.github.com>
2025-01-19 14:28:10 -0800

    Create page-about-us.php

---
+++ page-about-us.php
@@ -0,0 +1,55 @@
+<?php
+// ヘッダーを呼び出す
+get_header();
+?>
+
+<main class="about_us">
+    <section class="section Who_We_Are">
+        <h1 class="about_us_h1">Who We Are</h1>
+        <div class="Who_We_Are_body">
+            <p class="about_us_p">“Shibuly”, this name came from the location where our founding members met, Shibuya. <br>
+                Our founding members went to the same middle and high school.<br>
+                We love Japan, our home country.<br>
+                We believe Japan has more potential than the current situation.<br>
+                We are sure that Japan can contribute more to the World's more development and higher standard of living by people around the world becoming familiar with Japan on a daily basis.<br>
+                We named our company by connecting our meeting location and thoughts, 'Shibuya' and 'Daily'.<br>
+                We have no choice except standing up.<br>
+                We never give up.<br>
+                “Shibuly”, our story starts from this place to the world.
+            </p>
+        </div>
+    </section>
+    <section class="section Mission">
+        <h1 class="about_us_h1">Mission</h1>
+        <div class="Mission_contents">
+            <h2 class="about_us_h2_highlight">① Enriching The World by Japanese Technical Skills</h2>
+            <h2 class="about_us_h2_highlight">② Establishing Japanese Brand's Power Around The World</h2>
+            <h2 class="about_us_h2_highlight">③ Contributing to Increasing Japanese Producers' Profits</h2>
+        </div>
+    </section>
+    <section class="section Vision">
+        <h1 class="about_us_h1">Vision</h1>
+        <div class="Vision_contents">
+            <h2 class="about_us_h2">Becoming the Bridge between Japan and The World</h2>
+        </div>
+    </section>
+    <section class="section Value">
+        <h1 class="about_us_h1">Value</h1>
+        <div class="Value_contents">
+            <h2 class="about_us_h2">Contributing to The World by Contributing to Japan</h2>
+        </div>
+    </section>
+    <section class="section Shibuly_Business">
+        <h1 class="about_us_h1">Shibuly's Business</h1>
+        <div class="Shibuly_Business_contents">
+            <h2 class="about_us_h2">“Shibuly”, Our Japanese Cross Border E-Commerce</h2>
+            <p class="about_us_p">We provide our customers around the world with a variety of Japanese creditable products directly from Japanese producers to our customers through this electronic commerce. <br>
+                We will maximize the value for our customers of the products which Japanese producers work so hard to make.</p>
+        </div>
+    </section>
+</main>
+
+<?php
+// フッターを呼び出す
+get_footer();
+?>
